67d2fc0419b298b075ec2a8529199b050f2f8868
Daksh-14 <dakshmv14@gmail.com>
2024-02-27 23:57:09 +0530

    Nutrition page and features page

--- frontend/src/pages/Features.jsx
+++ frontend/src/pages/Features.jsx
@@ -1,37 +1,46 @@
 import React, { useEffect, useState } from 'react'
 import { NavLink } from "react-router-dom";
+import fsnap from "../assets/features_snap.jpg"
+import fChat from '../assets/features_chatbot.jpg'
+import fTrack from '../assets/tracker.jpg'
+import diet from '../assets/weekly_diet.png'
 import '../styles/features.css';
+import {BackButton} from '../components/BackButton';
 
 const featureArr = [
   {
     id:1,
     title : 'Food-Recipe',
     detail: 'Explore a variety of delicious recipes and cooking ideas to inspire your culinary adventures.',
-    to : '/features/foodrecipe'
+    to : '/features/foodrecipe',
   },
   {
     id:2,
     title : 'Nutrition',
     detail: 'Learn about healthy eating habits, nutritional information, and how to maintain a balanced diet.',
-    to : '/features/nutrition'
+    to : '/features/nutrition',
+    imgurl : diet
   },
   {
     id:3,
     title : 'AI Chatbot',
     detail: 'Engage with our AI-powered chatbot for quick answers, assistance, and personalized recommendations.',
-    to : '/features/chatbot'
+    to : '/features/chatbot',
+    imgurl : fChat
   },
   {
     id:4,
     title : 'Food Recognition',
     detail: 'Effortlessly identify various foods and ingredients using advanced image recognition technology.',
-    to : '/features/photoinfo'
+    to : '/features/photoinfo',
+    imgurl : fsnap
   },
   {
     id:5,
     title : 'Tracker',
     detail: 'Track your progress, set goals, and monitor your activities to stay motivated and achieve success.',
-    to : '/features/tracker'
+    to : '/features/tracker',
+    imgurl : fTrack
   },
 ];
 
@@ -50,7 +59,9 @@ export const Features = () => {
     }
 
   return (
-    <div>
+    <>
+    <BackButton />
+    <div className='feature_Container'>
      <div className="feature-main">
       <h1>Features</h1>
       <p>Discover the latest and greatest features now available! Our team is always working hard to bring you the best experience possible. With our newest updates, you'll be able to streamline your workflow, save time, and get more done. From advanced tools to improved user interfaces, our platform has everything you need to be more productive and efficient. Whether you're a seasoned user or new to our platform, you'll love what we've added. So check out our latest features and start exploring today!</p>
@@ -92,11 +103,13 @@ export const Features = () => {
           <div className='feature-clicked'>
             <h1>{feature.title}</h1>
             <p>{feature.detail}</p>
+            <img src={feature.imgurl} alt="" className='feature_img' />
             <NavLink to={feature.to}>Try Now!</NavLink>
           </div>
         }
       </div>
     </div>
+    </>
   )
 }
 
